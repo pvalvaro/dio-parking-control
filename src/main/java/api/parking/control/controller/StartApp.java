f37ff0e428a6b3883e9b57d19432dab22e369f89
pvalvaro <pvalvaro25@gmail.com>
2022-11-17 20:11:31 -0300

    hello 2

--- src/main/java/api/parking/control/controller/StartApp.java
+++ src/main/java/api/parking/control/controller/StartApp.java
@@ -1,12 +1,14 @@
 package api.parking.control.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("parking")		
+@RequestMapping("/")
 public class StartApp {
 	
+	@GetMapping
 	public String hello() {
 		return "DIO Parking Control";
 	}
