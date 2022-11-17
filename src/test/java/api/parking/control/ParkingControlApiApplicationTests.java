ea3da1d4f528e26341ae3f1b62d2a96aa29759d6
pvalvaro <pvalvaro25@gmail.com>
2022-11-17 19:41:44 -0300

    first commit

---
+++ src/test/java/api/parking/control/ParkingControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.parking.control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
